d1310dd5ff00a2a1a1bac9a0654f0d1192d1380b
Yousif Abdulmuti <158498454+Dev-Yousif@users.noreply.github.com>
2026-01-11 13:27:51 +0300

    first commit

---
+++ components/logo.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
+import { Unbounded } from "next/font/google";
+
+const unbounded = Unbounded({ subsets: ["latin"] });
+
+export const Logo = ({ className }: { className?: string }) => {
+  return (
+    <Link href="/" className={cn("flex items-center gap-2")}>
+      <LogoIcon />
+      <span className={cn("text-base font-semibold text-neutral-600 dark:text-neutral-200", unbounded.className)}>
+        Corteksa
+      </span>
+    </Link>
+  );
+};
+
+export const LogoIcon = (props: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div {...props} className={cn("relative", props.className)}>
+      <Image
+        src="/Corteksa.svg"
+        alt="Corteksa Logo"
+        width={28}
+        height={28}
+        className="w-7 h-7"
+      />
+    </div>
+  );
+};
